4047995fa985cf970d9be61bd39f7c53b477b417
niihen <admin@niihen.com>
2025-04-04 20:50:18 +0700

    Create hitou-meguri.js

---
+++ hitou-meguri.js
@@ -0,0 +1,7 @@
+var 
+  judul = `
+    <div>Hitou Meguri The Animation: Wakaokami, Rachiru</div><div>Japanese: 秘湯めぐり The Animation 「若女将、拉致る」</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">Yukino Chitose baru saja masuk ke Kunoe Girls School ketika ibunya kawin lari, meninggalkannya untuk mengurus dirinya sendiri dan mengelola penginapan onsen tempatnya tinggal. Tidak lama kemudian dia diperkosa oleh seorang turis dari Tokyo dan diculik oleh beberapa pria yang berusaha mengusirnya dari penginapannya.</div>
+  `;
