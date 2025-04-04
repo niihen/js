c63a205e3e0a651670e184b6f8789d813fbdd980
niihen <admin@niihen.com>
2025-04-04 11:35:00 +0700

    Create tsuma-no-haha-sayuri.js

---
+++ tsuma-no-haha-sayuri.js
@@ -0,0 +1,20 @@
+var 
+  judul = `
+    <div>Synonyms: My Wife's Mother Sayuri</div>
+    <div>Japanese: 妻の母さゆり</div>
+    <div>English: Sayuri's Mother-in-Law</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">
+    Akihito adalah karyawan kantoran yang sudah menikah namun belum punya anak.
+    Dia merasa frustasi karena masalah di kantor dengan bosnya dan juga istrinya
+    Akane yang sudah 3 hari belum pulang ke rumah. Ketika dia curhat kepada
+    bibinya Fujino, Fujino jadi curhat juga tentang suaminya yang tidak pernah
+    membelainya karena sibuk kerja. Esoknya Sayuri, Ibu mertuanya datang menemui
+    Akihito untuk memberikan sebuah surat cerai titipan putrinya dan surat
+    pepisahan karena telah punya pacar selingkuhan dan tinggal ke suatu tempat.
+    Merasa tak berhasil mendidik anak Sayuri kemudian bilang ke Akihito "Aku
+    akan menggantikan posisi Akane sebagai istri.." Akhirnya mereka bercinta
+    dengan penuh gairah.
+  </div>
+  `;
