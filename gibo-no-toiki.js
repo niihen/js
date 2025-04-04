82354e68cad044cdf7b1335c22007d13ab8ff8ee
niihen <admin@niihen.com>
2025-04-04 21:40:09 +0700

    Create gibo-no-toiki.js

---
+++ gibo-no-toiki.js
@@ -0,0 +1,7 @@
+var 
+  judul = `
+    <div>Gibo no Toiki: Haitoku Kokoro ni Tadayou Haha no Iroka</div><div><div>Japanese: 義母の吐息 ～背徳心に漂う母の色香～</div><div>English: Do You Know the Milfing Man?</div></div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">Ketika ibunya meninggal dan ayahnya menikah lagi, Kaito tersenyum kepada ibu tiri seksinya, Sayoko. Kejadian malam hari saat Kaito menyelinap ke kamar orang tuanya membuat Kaito bernafsu pada ibu tirinya, Beberapa kejadian tak terduga membuat Kaito lari dari rumah. Hingga pada malam hari saat penyelinapan dimulai membuat Sayoko dan Kaito memiliki hubungan khusus, Meski disiksa dengan rasa bersalah, Sayoko akhirnya menyerah pada birahinya. Seks, dosa dan pengkhianatan akan mengubah keseluruhan rumah Kaito!</div>
+  `;
