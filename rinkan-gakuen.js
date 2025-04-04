bf45df6200e41c6d814f28197d68d1c20913e1cd
niihen <admin@niihen.com>
2025-04-04 20:10:59 +0700

    Create rinkan-gakuen.js

---
+++ rinkan-gakuen.js
@@ -0,0 +1,7 @@
+var 
+  judul = `
+    <div style="text-align: left;">Rinkan Gakuen: Yamete! ...Okaasan, Minaide!</div><div style="text-align: left;"><div>Synonyms: Gangrape School</div><div>Japanese: 輪奸学園「やめてっ! …お母さん、見ないで!」</div></div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">Menceritakan tentang sebuah sekolah dengan murid yang semuanya laki-laki. di dalam sekolah terdapat seorang milf mantep yang menjadi guru wanita di sekolah tersebut, dengan adanya guru wanita di sekolah tersebut membuat murid-muridnya jadi ingin belajar tentang biologi. dan ada satu lagi cewek yang menjadi korban yaitu putri guru tersebut. Intinya guru diperkosa dan digangbang beserta anaknya di tempat berbeda.</div>
+  `;
