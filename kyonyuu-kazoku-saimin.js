2bd09ed3008ce0768cb18962ef25d0596be9026c
niihen <admin@niihen.com>
2025-04-04 13:46:00 +0700

    Create kyonyuu-kazoku-saimin.js

---
+++ kyonyuu-kazoku-saimin.js
@@ -0,0 +1,14 @@
+var 
+  judul = `
+    <div>Japanese: 巨乳家族催眠</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">
+    Tiba-tiba Ayahnya Yuuichi menikah lagi dengan istri barunya Mafuyu dan dua
+    anaknya yaitu Natsuki dan Akiha. Semenjak itu Yuuichi yang biasanya
+    sendirian kini telah mengalami perubahan dengan keluarga barunya.
+    Akhir-akhir ini Ayahnya sering bekerja ke luar negeri, disaat itulah Yuuichi
+    memesan “Senter Leser Hipnotis” untuk menghipnotis keluarganya dan melakukan
+    hubungan seks agar hubungan keluarga terasa lebih erat.
+  </div>
+  `;
