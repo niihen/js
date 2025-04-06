7a62e33cb2c9ce9846086da373738b1540225088
niihen <admin@niihen.com>
2025-04-07 04:27:24 +0700

    Update nonohara-yuzuka.js

--- nonohara-yuzuka.js
+++ nonohara-yuzuka.js
@@ -7,7 +7,7 @@ var
     <div style="text-align: justify;">
     Nonohara Yuzuka atau yang dipanggil Yuka adalah seorang siswi SMA introvert yang mempunyai sebuah rahasia, 
     dia suka siaran langsung sambil colmek untuk menemani para colikiawan internet. 
-    Hingga suatu haru dia mendapatkan sepucuk surat dari seseorang yang awalnya dikira surat cinta, 
+    Hingga suatu hari dia mendapatkan sepucuk surat dari seseorang yang awalnya dikira surat cinta, 
     tapi ternyata berisi adegan dia saat siaran langsung.
   </div>
   `;
