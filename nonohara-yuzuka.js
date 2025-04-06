eb708351ce8cc9c7b6142c1ec6279d0b06f221be
niihen <admin@niihen.com>
2025-04-07 04:20:35 +0700

    Create nonohara-yuzuka.js

---
+++ nonohara-yuzuka.js
@@ -0,0 +1,12 @@
+var 
+  judul = `
+    <div style="text-align: left;">Japanese: 野々原柚花のヒミツのハイシン</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">
+    Nonohara Yuzuka atau yang dipanggil Yuka adalah seorang siswi SMA introvert yang mempunyai sebuah rahasia, 
+    dia suka siaran langsung sambil colmek untuk menemani para colikiawan internet. 
+    Hingga suatu haru dia mendapatkan sepucuk surat dari seseorang yang awalnya dikira surat cinta, 
+    tapi ternyata berisi adegan dia saat siaran langsung.
+  </div>
+  `;
