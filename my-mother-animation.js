8cf38daf50fdaac98c7352589687aa1acd554955
niihen <admin@niihen.com>
2025-04-07 11:19:54 +0700

    Create my-mother-animation.js

---
+++ my-mother-animation.js
@@ -0,0 +1,15 @@
+var 
+  judul = `
+    <div style="text-align: left;">My Mother The Animation</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">
+    Sekian lama tidak berjumpa dengan ibunya Nush, kini Nont berlibur di hotel
+    bintang lima dekat pantai bersama ibunya. Mereka bersenang-senang dan
+    menikmati waktu bersama, hingga akhirnya ibunya mabuk berat karena
+    kebanyakan minum. Ibunya mendapatkan kabar bahwa Nont berhubungan seks
+    dengan saudara perempuannya sendiri, karena merasa cemburu dan efek dari
+    mabuk berat, ibunya terus menggoda Nont hingga akhirnya mereka berhubungan
+    seks untuk meluapkan rasa cinta mereka…
+  </div>
+  `;
