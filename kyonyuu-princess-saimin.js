0b3240ced71a36b24438cf47f4eed15ef6ba33b3
niihen <admin@niihen.com>
2025-04-18 20:15:06 +0700

    Create kyonyuu-princess-saimin.js

---
+++ kyonyuu-princess-saimin.js
@@ -0,0 +1,20 @@
+var 
+  judul = `
+    <div>Japanese: 巨乳プリンセス催眠</div>
+    <div>English: Busty Princess Hypnosis</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">
+    Faran adalah seorang laki-laki yang hendak menaklukkan kerajaan Crusch,
+    karena di masa lalu Raja Crusch telah menghancurkan kampung halamannya. Kini
+    kerajaan Crusch sedang membutuhkan pewaris laki-laki namun sang Raja tak
+    kunjung mendapat pewaris laki-laki hingga sang raja stress. Memanfaatkan
+    momen ini Faran menggunakan jimat pemberian seorang asasin wanita bernama
+    Kuzaha, jimat ini adalah sebuah telur yang dapat mengabulkan keinginan
+    apapun. Akhirnya Faran mendapatkan kemampuan sugesti mutlak atau bisa
+    dibilang hipnotis. Dia menggunakan kemampuannya untuk memanipulasi Ratu
+    Tamanna dan kedua putrinya yakni Farasha dan Safina. Faran meneruskan
+    rencana balas dendamnya hingga menghamili mereka semua dan mengendalikan
+    kerajaan sebagai Raja Baru.
+    </div>
+  `;
