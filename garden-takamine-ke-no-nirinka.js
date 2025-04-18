b008346a75aaa34607f4d87073255bc7ee4bcb1d
niihen <admin@niihen.com>
2025-04-18 08:44:49 +0700

    Create garden-takamine-ke-no-nirinka.js

---
+++ garden-takamine-ke-no-nirinka.js
@@ -0,0 +1,8 @@
+var 
+  judul = `
+      <div style="text-align: left;">Garden: Takamine-ke no Nirinka - The Animation</div>
+      <div style="text-align: left;">Japanese: Garden ~高嶺家の二輪花~ THE ANIMATION</div>
+  `,
+  sinopsis = `
+    <div style="text-align: justify;">Tomoya seorang anak muda yang diasuh oleh keluarga Takamine. Keluarga Takamine diantaranya ada sang tante Kasumi si milf yang bisa membuat anak muda ngiler melihat tubuh moleknya terutama Tomoya, dan ada kedua anak perempuan tante Kasumi yang bernama Ayane dan Sayuri yang juga memiliki bentuk tubuh yang montok disusia muda mereka, memang buah jatuh takkan jauh dari pohonnya.</div>
+  `;
